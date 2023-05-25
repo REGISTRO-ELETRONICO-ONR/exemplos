caa318c7d4fa46a2599f06f7e62276eaf238b968
ferrhcp2017 <ferrhcp@gmail.com>
2023-05-24 22:04:40 -0300

    Add Python3x Sample

---
+++ EXEMPLO_Geração_CNM_Python3.x/cnmgenerator.py
@@ -0,0 +1,22 @@
+import stdnum.iso7064.mod_97_10
+
+def calcMod97base10(num):
+    digVerificador = stdnum.iso7064.mod_97_10.calc_check_digits(num)
+    cnm = str(num) + str(digVerificador)
+    cns = cnm[0:6]
+    livro = cnm[6]
+    matricula = cnm[7:14]
+    matriculaCnm = cns + '.' + livro + '.' + matricula + '-' + digVerificador
+
+    return cns, matricula, matriculaCnm
+
+def CnmGenerator(cns, number):
+    matricula = str(number).zfill(7)
+    livro = 2
+    preCnm = cns + str(livro) + str(matricula)
+    getCnm = calcMod97base10(preCnm)
+
+    print (getCnm[2])
+    #return getCnm[2]
+
+CnmGenerator('999999', '1')
